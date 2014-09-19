d64ca908d9266314bc25821e1fa4fff00c75dbeb
Morten <ms.fuge@gmail.com>
2014-09-19 13:16:55 +0200

    rettelse

--- src/ghettosvend/GhettoSvend.java
+++ src/ghettosvend/GhettoSvend.java
@@ -18,6 +18,7 @@ public class GhettoSvend {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("GHETTOSVEND");
+        System.out.println("Snezananananana");
     }
     
 }
